76807785f766d02fb832aff5d0b148925084d401
gautam-4 <gautamalt468@gmail.com>
2024-09-18 02:32:52 +0530

    nav and readme update

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cerebro | All-in-One Productivity App
+
+A fully responsive productivity app built with **Next.js 14** and **Supabase** that includes the following features:
+- **Google OAuth Authentication**
+- **Todo List**
+- **Habit Tracker** 
+- **Expense Tracker**
+- **Pomodoro Timer**
+
+## Live Demo
+
+Check out the live app here: [Cerebro Productivity App](https://cere-bro.vercel.app/)
+
+## Screenshot
+
+![Cerebro App Screenshot](./screenshot.png)
--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -43,18 +43,18 @@ const Navbar = () => {
                     {user ? (
                         <button
                             onClick={handleLogout}
-                            className="flex justify-around px-4 py-1 border-0 font-bold text-sm rounded-lg navbar-btn-red"
+                            className="flex gap-2.5 px-5 py-1 justify-center items-center border-0 font-bold text-sm rounded-lg navbar-btn-red"
                         >
                             <Image src={logoutIcon} alt="logout" />
-                            <span className="hidden sm:block">&nbsp;Log Out</span>
+                            <span className="hidden sm:block text-lg">&nbsp;Log Out</span>
                         </button>
                     ) : (
                         <button
-                            className="flex justify-around px-4 py-1 border-0 font-bold text-sm rounded-lg navbar-btn-green"
+                            className="flex gap-2.5 px-5 py-1 justify-center items-center border-0 font-bold text-sm rounded-lg navbar-btn-green"
                             onClick={handleGoogleAuth}
                         >
                             <Image src={logoutIcon} alt="logout" />
-                            <span className="hidden sm:block">Log In</span>
+                            <span className="hidden sm:block text-lg">Log In</span>
                         </button>
                     )}
                 </div>
