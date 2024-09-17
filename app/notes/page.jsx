7cb59d5d9cb97c647a4915b388f15a545b536e2e
gautam-4 <gautamalt468@gmail.com>
2024-09-17 14:50:47 +0530

    add auth

--- app/notes/page.jsx
+++
@@ -1,9 +0,0 @@
-function Notes() {
-  return (
-    <>
-    
-    </>
-  )
-}
-
-export default Notes
--- components/auth/register.jsx
+++
@@ -1,9 +0,0 @@
-function Register() {
-  return (
-    <>
-    
-    </>
-  )
-}
-
-export default Register
