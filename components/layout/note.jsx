bf729fe78ae02baf4ab9ff0e75ee94cf7a324137
gautam-4 <gautamalt468@gmail.com>
2025-01-30 22:07:16 +0530

    change notes placeholder

--- components/layout/note.jsx
+++ components/layout/note.jsx
@@ -167,7 +167,7 @@ function Note() {
       <textarea
         name="note"
         id="note"
-        placeholder="Start typing your note..."
+        placeholder="Note"
         className="bg-note_color p-3 font-cursive text-lg rounded-xl border-0 resize-none text-gray-800 w-full h-52 focus:ring-2 focus:ring-blue-500 focus:outline-none"
         value={note}
         onChange={handleChange}
